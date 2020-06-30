6a64829acf30bc569ce325973eb256f287d55090
jpfs <jpfs@gft.com>
2020-06-30 19:17:41 +0200

    fix: add esm for tree shaking

--- rollup.config.js
+++ rollup.config.js
@@ -2,23 +2,34 @@ import esbuild from 'rollup-plugin-esbuild';
 import autoExternal from 'rollup-plugin-auto-external';
 import pkg from './package.json';
 
-export default {
-  input: 'src/index.ts',
-  treeshake: true,
-  output: {
-    sourcemap: false,
-    format: 'cjs',
-    file: pkg.main
-  },
-  plugins: [
-    autoExternal({
-      builtins: true,
-      peerDependencies: true,
-      dependencies: true
-    }),
-    esbuild({
-      minify: false,
-      target: 'esnext'
-    })
-  ]
+export default () => {
+  const mainInput = {
+    input: 'src/index.ts',
+    treeshake: true,
+    output: {
+      sourcemap: false,
+      format: 'cjs',
+      file: pkg.main
+    },
+    plugins: [
+      autoExternal({
+        builtins: true,
+        peerDependencies: true,
+        dependencies: true
+      }),
+      esbuild({
+        minify: false,
+        target: 'esnext'
+      })
+    ]
+  };
+  const esmInput = {
+    ...mainInput,
+    output: {
+      sourcemap: false,
+      format: 'esm',
+      file: pkg.module
+    }
+  };
+  return [mainInput, esmInput];
 };
